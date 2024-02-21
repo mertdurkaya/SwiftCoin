8657c03a8a8ad4fdb53e14f51c0f909f1e225a6b
Mert Durkaya <mertdurkaya@gmail.com>
2024-02-21 20:17:57 +0300

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Mert Durkaya on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
