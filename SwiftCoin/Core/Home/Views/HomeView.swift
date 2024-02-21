d2c81db0a14fad4e5b2c58a0dcc96e786b19c489
Mert Durkaya <mertdurkaya@gmail.com>
2024-02-21 20:30:40 +0300

    Project Setup

---
+++ SwiftCoin/Core/Home/Views/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  SwiftCoin
+//
+//  Created by Mert Durkaya on 21/02/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // top movers
+                
+                // all coins
+                
+                
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
